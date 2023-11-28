ce921f59b50962c6eb80e2a45cb1bd98ea466b38
Hugo <>
2023-11-28 14:56:11 +0100

    feat: implement the base of signin and signup page

---
+++ lib/ui/account/signup.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inscription'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Ajoutez ici la logique de vérification du login
+                String email = _emailController.text;
+                String password = _passwordController.text;
+                print('Email: $email\nPassword: $password');
+                // Ajoutez la logique de vérification du login ici
+              },
+              child: const Text('Inscription'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
