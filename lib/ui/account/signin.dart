4dd313981377667576943542abd6083814ca48ea
Guilhem COULY <couly.guilhem@gmail.com>
2023-11-30 23:01:00 +0100

    fix bad merge from myself

--- lib/ui/account/signin.dart
+++ lib/ui/account/signin.dart
@@ -27,75 +27,86 @@ class _SigninPageState extends State<SigninPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Connexion'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Bibliz"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: "Nom d'utilisateur"),
+            const Text(
+              "Connexion",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Mot de passe'),
-              obscureText: true,
+            const SizedBox(height: 32.0),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: TextField(
+                controller: _usernameController,
+                decoration:
+                const InputDecoration(labelText: "Nom d'utilisateur"),
+              ),
             ),
             const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // Ajoutez ici la logique de vérification du login
-                String username = _usernameController.text;
-                String password = sha256
-                    .convert(utf8.encode(_passwordController.text))
-                    .toString();
-                UserQuery().signin(username, password).then((value) async {
-                  await SharedPrefs().setCurrentUser(username);
-                  if (context.mounted) {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HomePage()));
-                  }
-                }).catchError((error) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(error.toString()),
-                      duration: const Duration(seconds: 3),
-                    ),
-                  );
-                });
-              },
-              child: const Text('Connexion'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SignUpPage()));
-              },
-              child: const Text("S'inscrire"),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Mot de passe'),
+                obscureText: true,
+              ),
             ),
             const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () async {
-                String titleToSearch =
-                    "Exemple de Titre"; // Remplacez par le titre à rechercher
-                Book? book = await bookQuery.getBookByTitle(titleToSearch);
-
-                if (book != null) {
-                  print("Livre trouvé: ${book.title}");
-                  // Vous pouvez aussi afficher les détails du livre ici ou sur une nouvelle page
-                } else {
-                  print("Aucun livre trouvé avec ce titre");
-                }
-              },
-              child: const Text("Rechercher un Livre par Titre"),
-            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Ajoutez ici la logique de vérification du login
+                      String username = _usernameController.text;
+                      String password = sha256
+                          .convert(utf8.encode(_passwordController.text))
+                          .toString();
+                      UserQuery()
+                          .signin(username, password)
+                          .then((value) async {
+                        await SharedPrefs().setCurrentUser(username);
+                        if (context.mounted) {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const HomePage()));
+                        }
+                      }).catchError((error) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(error.toString()),
+                            duration: const Duration(seconds: 3),
+                          ),
+                        );
+                      });
+                    },
+                    child: const Text('Connexion'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUpPage()));
+                    },
+                    child: const Text("S'inscrire"),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
